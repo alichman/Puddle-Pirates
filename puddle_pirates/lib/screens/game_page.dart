d962ef98fc47bcde7c3300794579a7b2ed124290
alichman <alichman@uoguelph.ca>
2025-03-20 17:43:01 -0400

    Move setup page to new screen, minor tweaks

--- puddle_pirates/lib/screens/game_page.dart
+++ puddle_pirates/lib/screens/game_page.dart
@@ -1,99 +1,9 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:puddle_pirates/battleship.dart';
-import 'package:puddle_pirates/states.dart';
 
 
-class GameSetupPage extends StatefulWidget {
-  const GameSetupPage({super.key});
 
-  @override
-  State<GameSetupPage> createState() => _GameSetupState();  
-}
-
-class _GameSetupState extends State<GameSetupPage> {
-  bool callbackSuccess = false;
-  final cPlayer = globalGameState.getCurrentPlayer();
-  // Ship types to still be created.
-  // Placed ships will be stored in the grid.
-  // Map notation to make a mutable copy.
-  final ships = ShipType.values.map((t) => t).toList(); 
-  int? selected;
-  bool vert = false;
-
-  @override
-  Widget build(BuildContext context) {
-    const shipSize = 50;
-
-    return Column(
-      children: [
-        // Grid
-        ChangeNotifierProvider.value(
-          value: cPlayer.grid,
-          child: Expanded(child: 
-            Center(child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.95, // Take up 80% of the screen width.
-              child: BattleshipGrid(callback: (square) {
-                if (selected == null) {
-                  return;
-                }
-                // Try placing ship
-                try {
-                  cPlayer.grid.addShip(ships[selected!], square, vert);
-                  setState(() {
-                    callbackSuccess = true;
-                    ships.removeAt(selected!);
-                    selected = null;
-                  });
-                } catch (e) {print(e);}
-              },)
-            )))
-        ),
-        // Ship selector
-        Expanded(child: Column(
-          children: ships.map((s) => GestureDetector(
-            onTap: () {
-              setState(() => selected = ships.indexOf(s));
-            },
-            child: SizedBox(
-            height: shipSize.toDouble(),
-            width: (shipSize * shipLengthMap[s]!).toDouble(),
-            child: Container(
-              color: ships.indexOf(s) == selected ? const Color.fromARGB(255, 43, 99, 45) : Colors.green,
-              alignment: Alignment.center,
-              margin: EdgeInsets.all(1),
-            )))).toList(),
-          )),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-            ),
-            onPressed: () {
-              if (ships.isNotEmpty) return;
-              Navigator.pushNamed(context, '/game_page');
-            },
-            child: const Text(
-              "Continue",
-              style: TextStyle(fontSize: 16),
-            ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-            ),
-            onPressed: () {
-              Navigator.pushNamed(context, '/game_creation');
-            },
-            child: const Text(
-              "Back to Game Creation",
-              style: TextStyle(fontSize: 16),
-            ),
-          ),
-      ]);
-  }
-}
 
 class GamePage extends StatelessWidget {
   const GamePage({super.key});
--- puddle_pirates/lib/states.dart
+++ puddle_pirates/lib/states.dart
@@ -10,13 +10,20 @@ class GameState extends ChangeNotifier {
   List<Player> players = [];
 
   // No AI Support yet.
+  // Also resets game values.
   void setNewPlayers(String p1Name, String p2Name) {
     players = [Player(p1Name), Player(p2Name)];
+    cPlayer = 0;
+    round = 0;
     notifyListeners();
   }
 
   Player getCurrentPlayer () => players[cPlayer];
-  
+  void toNextPlayer() {
+    // 1 - 0 = 1, 1 - 1 = 0
+    cPlayer = 1 - cPlayer;
+    notifyListeners();
+  }
 }
 
 GameState globalGameState = GameState();
